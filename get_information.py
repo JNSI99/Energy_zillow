d976b919d26236fc359806bb68a049a3b1e47dde
JimmyNSI <jnsi4785@gmail.com>
2026-04-16 00:22:12 -0400

    Script for basic information

---
+++ get_information.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+"""
+Creates a CSV that gives you basic information
+about the building.
+"""
+
+
+df = pd.read_csv("merged.csv", low_memory=False)
+
+df_test_fuels = df[["BBL",
+                    "Address 1",
+                    "address",
+                    "Property Name"
+                    ]].copy()
+
+df_test_fuels.to_csv("basic_information.csv",index = False,encoding='utf-8')
