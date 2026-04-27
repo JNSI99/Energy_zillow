429293854df074f06a23112048f3aa985381a617
JimmyNSI <jnsi4785@gmail.com>
2026-04-26 20:30:01 -0400

    .sh updated

--- startup.sh
+++ startup.sh
@@ -2,13 +2,20 @@
 
 echo "Running ETL pipeline..."
 
-python get_datasets.py
-python get_transform.py
-python get_fuels.py
-python get_information.py
-python get_predictions.py
-python get_ranking.py
 
+echo "Running get_datasets.py"
+python get_datasets.py
+echo "Running get_transform.py"
+python get_transform.py
+echo "Running get_fuels.py"
+python get_fuels.py
+echo "Running get_information.py"
+python get_information.py
+echo "Running get_predictions.py"
+python get_predictions.py
+echo "Running get_ranking.py"
+python get_ranking.py
+echo "All scripts executed successfully."
 
 echo "Starting Streamlit..."
 
